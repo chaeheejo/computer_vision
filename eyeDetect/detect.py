28c6050cae2ff9303b22a59b96fddf2f3a5e9f95
chaeheejo <60cogml@gmail.com>
2022-05-11 23:41:52 +0900

    eyeDetection at local system

---
+++ eyeDetect/detect.py
@@ -0,0 +1,105 @@
+import cv2
+import dlib
+import numpy as np
+import torch
+from model import Map
+from imutils import face_utils
+
+IMG_SIZE = (34,26)
+PATH = './weights/classifier_weights_iter_50.pt'
+
+model = Map()
+model.load_state_dict(torch.load(PATH))
+model.eval()
+
+def predict(pred):
+  pred = pred.transpose(1, 3).transpose(2, 3)
+  outputs = model(pred)
+  pred_tag = torch.round(torch.sigmoid(outputs))
+
+  return pred_tag
+
+def crop_eye(img, eye_points):
+  x1, y1 = np.amin(eye_points, axis=0)
+  x2, y2 = np.amax(eye_points, axis=0)
+  cx, cy = (x1 + x2) / 2, (y1 + y2) / 2
+
+  w = (x2 - x1) * 1.2
+  h = w * IMG_SIZE[1] / IMG_SIZE[0]
+
+  margin_x, margin_y = w / 2, h / 2
+
+  min_x, min_y = int(cx - margin_x), int(cy - margin_y)
+  max_x, max_y = int(cx + margin_x), int(cy + margin_y)
+
+  eye_rect = np.rint([min_x, min_y, max_x, max_y]).astype(np.int)
+  eye_img = gray[eye_rect[1]:eye_rect[3], eye_rect[0]:eye_rect[2]]
+
+  return eye_img, eye_rect
+
+capture = cv2.VideoCapture(0)
+
+if capture.isOpened()==False:
+  raise Exception("카메라 연결 안됨")
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor('./weights/shape_predictor_68_face_landmarks.dat')
+
+n_count = 0
+
+while True:
+  ret, frame = capture.read()
+  gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+  faces = detector(gray)
+
+  for face in faces:
+    shapes = predictor(gray, face)
+    shapes = face_utils.shape_to_np(shapes)
+
+    eye_img_l, eye_rect_l = crop_eye(gray, eye_points=shapes[36:42])
+    eye_img_r, eye_rect_r = crop_eye(gray, eye_points=shapes[42:48])
+
+    eye_img_l = cv2.resize(eye_img_l, dsize=IMG_SIZE)
+    eye_img_r = cv2.resize(eye_img_r, dsize=IMG_SIZE)
+    eye_img_r = cv2.flip(eye_img_r, flipCode=1)
+
+    cv2.imshow('left_eye', eye_img_l)
+    cv2.imshow('right_eye', eye_img_r)
+
+    eye_input_l = eye_img_l.copy().reshape((1, IMG_SIZE[1], IMG_SIZE[0], 1)).astype(np.float32)
+    eye_input_r = eye_img_r.copy().reshape((1, IMG_SIZE[1], IMG_SIZE[0], 1)).astype(np.float32)
+
+    eye_input_l = torch.from_numpy(eye_input_l)
+    eye_input_r = torch.from_numpy(eye_input_r)
+
+    pred_l = predict(eye_input_l)
+    pred_r = predict(eye_input_r)
+    print(pred_l, pred_r)
+
+    state_l = 'O %.1f' if pred_l > 0.0 else '- %.1f'
+    state_r = 'O %.1f' if pred_r > 0.0 else '- %.1f'
+
+    state_l = state_l % pred_l
+    state_r = state_r % pred_r
+
+    if pred_l.item() == 0.0 and pred_r.item() == 0.0:
+      n_count+=1
+
+    else:
+      n_count = 0
+
+    if n_count > 50:
+      cv2.putText(frame,"Wake up", (120,160), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255),2)
+
+    cv2.rectangle(frame, pt1=tuple(eye_rect_l[0:2]), pt2=tuple(eye_rect_l[2:4]), color=(255,255,255), thickness=2)
+    cv2.rectangle(frame, pt1=tuple(eye_rect_r[0:2]), pt2=tuple(eye_rect_r[2:4]), color=(255,255,255), thickness=2)
+
+    cv2.putText(frame, state_l, tuple(eye_rect_l[0:2]), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+    cv2.putText(frame, state_r, tuple(eye_rect_r[0:2]), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+
+
+  if not ret : break
+  if cv2.waitKey(30)>=0: break
+  title = "Wake up Program"
+  cv2.imshow(title, frame)
+capture.release()
---
+++ eyeDetect/train.py
@@ -0,0 +1,75 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+from torchvision.transforms import transforms
+from torch.utils.data import DataLoader
+from dataclass import eyes_dataset
+from model import Map
+import torch.optim as optim
+
+torch.set_num_threads(1)
+
+x_train = np.load('./dataset/x_train.npy').astype(np.float32)  # (2586, 26, 34, 1)
+y_train = np.load('./dataset/y_train.npy').astype(np.float32)  # (2586, 1)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+train_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.RandomRotation(10),
+    transforms.RandomHorizontalFlip(),
+])
+
+train_dataset = eyes_dataset(x_train, y_train, transform=train_transform)
+
+def accuracy(y_pred, y_test):
+    y_pred_tag = torch.round(torch.sigmoid(y_pred))
+
+    correct_results_sum = (y_pred_tag == y_test).sum().float()
+    acc = correct_results_sum / y_test.shape[0]
+    acc = torch.round(acc * 100)
+
+    return acc
+
+PATH = 'weights/classifier_weights_iter_50.pt'
+
+train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=4)
+
+model = Map()
+model.to(device)
+
+criterion = nn.BCEWithLogitsLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.0001)
+
+epochs = 50
+
+for epoch in range(epochs):
+    running_loss = 0.0
+    running_acc = 0.0
+
+    model.train()
+
+    for i, data in enumerate(train_dataloader, 0):
+        input_1, labels = data[0].to(device), data[1].to(device)
+
+        input = input_1.transpose(1, 3).transpose(2, 3)
+
+        optimizer.zero_grad()
+
+        outputs = model(input)
+
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        running_acc += accuracy(outputs, labels)
+
+        if i % 80 == 79:
+            print('epoch: [%d/%d] train_loss: %.5f train_acc: %.5f' % (
+                epoch + 1, epochs, running_loss / 80, running_acc / 80))
+            running_loss = 0.0
+
+print("learning finish")
+torch.save(model.state_dict(), PATH)
