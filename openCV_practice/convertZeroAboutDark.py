a75f3f74fc44a2bb0c5d6627d35ec82497676fa7
chaeheejo <60cogml@gmail.com>
2022-10-10 00:36:59 +0900

    plus error condition convert zero about dark part

--- openCV_practice/convertZeroAboutDark.py
+++ openCV_practice/convertZeroAboutDark.py
@@ -4,8 +4,7 @@ import cv2 as cv
 img = cv.imread('sample.jpg', cv.IMREAD_GRAYSCALE)
 
 if img is None:
-    print('Image load failed')
-    exit()
+    raise Exception("Image load failed")
 
 mean = np.mean(img)
 temp = img<mean
@@ -13,7 +12,7 @@ result = img[:]
 
 for i in range(len(temp)):
     for j in range(len(temp[0])):
-        if temp[i][j]==True:
+        if temp[i][j]:
             result[i][j] = 0
 
 cv.imwrite('output.jpg', result)
