9c3eb07341964f575fb3d7e228ff7e41c09b94f2
chaeheejo <60cogml@gmail.com>
2022-10-11 19:34:45 +0900

    plus inverse frame maintain effect after 3 second

--- openCV_practice/inverseFrame.py
+++ openCV_practice/inverseFrame.py
@@ -5,36 +5,44 @@ cap = cv.VideoCapture(0)
 
 w = round(cap.get(cv.CAP_PROP_FRAME_WIDTH))
 h = round(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
-fps = cap.get(cv.CAP_PROP_FPS)
+fps = 15
 
 fourcc = cv.VideoWriter_fourcc(*'MJPG')
-output_video = cv.VideoWriter('output.avi', fourcc, fps, (w, h))
+output_video = cv.VideoWriter('output.avi', fourcc, fps, (w, h), 0)
 
 if not output_video.isOpened():
     raise Exception("File open failed")
 
 last_frame = 0
-
+frame_cnt=0
 while True:
     ret, frame = cap.read()
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    cur_frame = np.mean(gray)
-
-    if last_frame == 0:
-        last_frame = cur_frame
 
     if not ret:
         break
 
-    if abs(cur_frame - last_frame) > 30:
-        inverse = ~frame
-        output_video.write(inverse)
+    if 0<frame_cnt:
+        frame_cnt-=1
+        output_video.write(~gray)
+        frame = ~gray
     else:
-        output_video.write(frame)
+        cur_frame = np.mean(gray)
 
-    last_frame = cur_frame
+        if last_frame == 0:
+            last_frame = cur_frame
 
-    if cv.waitKey(round(1000/fps)) >=0 :
+        if abs(cur_frame - last_frame) > 30:
+            frame_cnt=fps*3-1
+            output_video.write(~gray)
+            frame = ~gray
+        else:
+            output_video.write(gray)
+            frame = gray
+
+        last_frame = cur_frame
+
+    if cv.waitKey(25) >=0 :
       break
 
     cv.imshow('task3',frame)
